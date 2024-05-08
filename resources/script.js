bbc75d86f6a225b5e321207fd596a78540f937bc
Polina Gusakova <poll.gusakova@gmail.com>
2024-05-08 21:07:44 +0300

    Added dark mode and footer

--- resources/script.js
+++ resources/script.js
@@ -1,9 +1,6 @@
-function myFunction() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
+let eventTarget = document.getElementById("theme");
+let root = document.querySelector(":root");
 
-    let icon = document.getElementsByClassName('hero_link');
-    for (let i = 0; i < icon.length; i++) {
-        icon[i].style.color = 'white';
-    }
-}
+eventTarget.addEventListener('click', () => {
+    root.classList.toggle('dark');
+})
